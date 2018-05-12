3d9e8567d298e7d7f9317d94aa7dcd8d25497481
Dragon12346 <league@WTS14-2.attlocal.net>
2018-05-11 18:27:40 -0700

    finish arrays

--- src/_01_array_manipulation/Inserting.java
+++ src/_01_array_manipulation/Inserting.java
@@ -3,5 +3,15 @@ package _01_array_manipulation;
 
 
 public class Inserting {
+
+	public static int[] insertAt(int[] testArray, int x, int y) {
+		// TODO Auto-generated method stub
+		 int[] TestArray= {testArray.length+1};
+		 for (int i = 0; i < TestArray.length; i++) {
+			TestArray[i]+=testArray[i];
+		}
+		TestArray[x]=y;
+		return TestArray;
+	}
 	
 }
--- src/_01_array_manipulation/Sorting.java
+++ src/_01_array_manipulation/Sorting.java
@@ -2,4 +2,23 @@ package _01_array_manipulation;
 
 public class Sorting {
 
+	public static void sort(String[] testArray) {
+		// TODO Auto-generated method stub
+		boolean b=true;
+		while (b) {
+			b=false;
+			for (int i = 0; i < testArray.length-1; i++) {
+				int j=testArray[i].compareTo(testArray[i+1]);
+				if (Math.abs(j)==j) {
+					String s= testArray[i];
+					testArray[i]=testArray[i+1];
+					testArray[i+1]=s;
+					b=true;
+				}
+			}
+			
+		}
+		
+	}
+
 }
